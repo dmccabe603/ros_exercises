bcf0074cd8ea0d61f8602e3a60771ff244c7a40c
dmccabe603 <dmccabe@mit.edu>
2023-02-14 16:33:06 +0000

    simple publisher

---
+++ simple_publisher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float32
+import random
+
+def talker():
+    pub = rospy.Publisher('my_random_float', Float32, queue_size=10)
+    rospy.init_node('simple_publisher', anonymous=True)
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        rand_num = 10*random.random() % rospy.get_time()
+        rospy.loginfo(rand_num)
+        pub.publish(rand_num)
+        rate.sleep()
+ 
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInteruptException:
+        pass
