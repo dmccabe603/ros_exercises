d1e8260d342eb556c5cce4c027b43652c8c9a898
dmccabe603 <dmccabe@mit.edu>
2023-02-15 21:34:29 +0000

    new subscriber

--- scripts/simple_subscriber.py
+++ scripts/simple_subscriber.py
@@ -3,13 +3,20 @@ import rospy
 from std_msgs.msg import Float32
 import numpy as np
 
-def callback(data):
-    rospy.loginfo(np.log(data), loger_name="random_float_log")
-
 def simple_subscriber():
     rospy.init_node('simple_subscriber', anonymous=False)
+    pub = rospy.Publisher("random_float_log", Float32, queue_size=10)
+
+    def callback(data):
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            pub.publish(np.log(data.data))
+            rospy.loginfo(np.log(data.data))
+            rate.sleep()
+    
     rospy.Subscriber('my_random_float', Float32, callback)
+    
     rospy.spin()
 
 if __name__ == '__main__':
-        simple_subscriber()
+    simple_subscriber()
